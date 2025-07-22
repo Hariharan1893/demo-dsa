6a603d9c3722fdb0fbdb5d73fe95b560306cf28b
HariR1893 <hariharanr1893@gmail.com>
2025-07-22 18:32:44 +0530

    Arrays changed

--- arrays/ArrayOperations.java
+++ arrays/ArrayOperations.java
@@ -6,15 +6,9 @@ import java.util.List;
 
 class ArrayOperations {
 
-  static int min(int[] arr) {
-    int min = Integer.MAX_VALUE;
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] < min) {
-        min = arr[i];
-      }
-    }
-    return min;
-  }
+  
+
+  // new line added
 
   static int max(int[] arr) {
     int max = Integer.MIN_VALUE;
